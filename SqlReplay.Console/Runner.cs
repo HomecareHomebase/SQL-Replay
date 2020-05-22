e33727a7f1bbf86e4ae21a5a9f5199fcebf343e7
Eric.Jensen <Eric.Jensen@hearst.com>
2020-05-21 23:28:46 -0500

    Keep events in same transaction together even with no begin transaction

--- SqlReplay.Console/Runner.cs
+++ SqlReplay.Console/Runner.cs
@@ -94,9 +94,12 @@
 
             Console.WriteLine("Preparing 15 second buckets...");
 
+            //Dependent events are associated to a transaction but aren't the begin transaction
             var allDependentEvents = allEvents.Where(e => e.TransactionId != "0" && ((e is Rpc) || (e as Transaction)?.TransactionState != "Begin")).ToList();
+            //Independent events have no transaction or are a begin transaction
             var allIndependentEvents = allEvents.Where(e => e.TransactionId == "0" || (e as Transaction)?.TransactionState == "Begin");
 
+            //Split independent events up into buckets of 15 second intervals based on their Timestamp
             var groups = allIndependentEvents.GroupBy(r => r.Timestamp.ToString("ddhhmm") + r.Timestamp.Second / 15).OrderBy(g => g.Key);
             var buckets = new Dictionary<string, List<Event>>();
             foreach (var group in groups)
@@ -110,8 +113,8 @@
                     startDict.Add(trx.TransactionId, trx);
                 }
 
+                //Assign dependent events to their begin transaction
                 var bucket = new List<Event>(group);
-
                 foreach(var evt in allDependentEvents)
                 {
                     if (startHash.Contains(evt.TransactionId))
@@ -119,58 +122,57 @@
                         startDict[evt.TransactionId].Events.Add(evt);                        
                     }                    
                 }
-                
+
+                //Throw away any begin transactions that don't have anything associated with it
+                trxStarts.RemoveAll(t => t.Events.Count == 0);
+
+                //Order transaction's events by EventSequence and make sure they close
                 foreach (var trx in trxStarts)
                 {
                     trx.Events = trx.Events.OrderBy(e => e.EventSequence).ToList();
-                    //Avoid stragglers at end of trace that don't include a commit or rollback
-                    if (!trx.Events.Any(e => (e as Transaction)?.TransactionState == "Commit" ||
-                                             (e as Transaction)?.TransactionState == "Rollback"))
-                    {
-                        Event model;
-                        if (trx.Events.Count > 0)
-                        {
-                            model = trx.Events.Last();
-                        }
-                        else
-                        {
-                            model = trx;
-                        }
-                        trx.Events.Add(new Transaction
-                        {
-                            TransactionId = model.TransactionId,
-                            TransactionState = "Commit",
-                            EventSequence = model.EventSequence + 1,
-                            Timestamp = model.Timestamp.AddMilliseconds(1)
-                        });
-                    }
+                    CloseTransactionIfOpen(trx);
                 }
-
                 buckets.Add(group.Key, bucket);              
             }
 
+            //Identify orphaned events with no begin transaction
             var bucketDependentEvents = buckets.SelectMany(b => b.Value.Where(i => i is Transaction).SelectMany(i => (i as Transaction)?.Events));
-            var orphanEvents = allDependentEvents.Except(bucketDependentEvents);
-            foreach (var evt in orphanEvents)
+            var orphanEvents = allDependentEvents.Except(bucketDependentEvents).ToList();
+            foreach (var group in orphanEvents.GroupBy(e => e.TransactionId))
             {
-                string key = evt.Timestamp.ToString("ddhhmm") + evt.Timestamp.Second / 15;
-                if (buckets.TryGetValue(key, out var events))
+                //Make orphans a begin transaction and order them by EventSequence
+                var transaction = new Transaction
                 {
-                    events.Add(evt);
+                    Events = group.OrderBy(e => e.EventSequence).ToList()
+                };
+                var firstEvt = transaction.Events.First();
+                transaction.TransactionId = firstEvt.TransactionId;
+                transaction.TransactionState = "Begin";
+                transaction.EventSequence = firstEvt.EventSequence - 1;
+                transaction.Timestamp = firstEvt.Timestamp.AddMilliseconds(1);
+                //Make sure this new transaction will close
+                CloseTransactionIfOpen(transaction);
+
+                //Find (or create) a bucket for this new transaction
+                var key = transaction.Timestamp.ToString("ddhhmm") + transaction.Timestamp.Second / 15;
+                if (buckets.TryGetValue(key, out var bucket))
+                {
+                    bucket.Add(transaction);
                 }
                 else
                 {
-                    buckets.Add(key, new List<Event> { evt });
+                    buckets.Add(key, new List<Event> { transaction });
                 }
             }
 
-            var orderedBuckets = buckets.OrderBy(b => b.Key).Select(b => b.Value.OrderBy(e => e.Timestamp).ToList()).ToList();
+            //Order the buckets by key and all their items by Timestamp since EventSequence is only reliable within a transaction
+            var orderedBuckets = buckets.OrderBy(b => b.Key).Select(b => b.Value.OrderBy(e => e.Timestamp).ToList()).ToList();       
 
             Console.WriteLine("Kicking off executions...");
 
-            List<Task> tasks = new List<Task>();
+            var tasks = new List<Task>();
             var eventExecutor = new EventExecutor();
-            DateTimeOffset replayOrigin = DateTimeOffset.UtcNow;
+            var replayOrigin = DateTimeOffset.UtcNow;
 
             foreach (var bucket in orderedBuckets)
             {
@@ -186,6 +188,21 @@
             this.Exceptions.AddRange(eventExecutor.Exceptions);
         }
 
+        private void CloseTransactionIfOpen(Transaction transaction)
+        {
+            //Close open transactions at end that don't include a commit or rollback
+            if (transaction.Events.Any(e => (e as Transaction)?.TransactionState == "Commit" ||
+                                            (e as Transaction)?.TransactionState == "Rollback")) return;
+            var lastEvt = transaction.Events.Last();
+            transaction.Events.Add(new Transaction
+            {
+                TransactionId = lastEvt.TransactionId,
+                TransactionState = "Commit",
+                EventSequence = lastEvt.EventSequence + 1,
+                Timestamp = lastEvt.Timestamp.AddMilliseconds(1)
+            });
+        }
+
         private void MakeRpcCallsWithTvpParametersSqlBatchCalls(IEnumerable<Event> events)
         {
             //Remove procedure name and parameters so proc calls with TVP variables will get executed as SQLBatch instead of RCP and get plan stored in cache
@@ -197,7 +214,7 @@
                     rpc.Procedure = null;
                     rpc.Parameters.Clear();
                 }
-}
+            }
         }
 
         private void DuplicateEvents(Run run, byte factor)
