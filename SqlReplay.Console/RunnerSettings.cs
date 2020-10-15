d30d140badc90da2b828b02f43d12b19982f9cb1
Manjula Paladugu <mpaladugu@hchb.com>
2020-10-15 09:12:48 -0400

    Command timeout and other settings configurable

---
+++ SqlReplay.Console/RunnerSettings.cs
@@ -0,0 +1,18 @@
+﻿
+namespace SqlReplay.Console
+{
+    internal class RunnerSettings: IRunnerSettings
+    {
+        /// <inheritdoc />
+        public int SqlCommandTimeout { get; set; } = 1800;
+
+        /// <inheritdoc />
+        public int BulkCopyTimeout { get; set; } = 1800;
+
+        /// <inheritdoc />
+        public int TransactionScopeTimeout { get; set; } = 3600;
+
+        /// <inheritdoc />
+        public int BucketInterval { get; set; } = 15;
+    }
+}
